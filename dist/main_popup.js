6128060cc9ff3ae94fd58565019ae5b6efb8dd93
SvvPhenex <150884314+Phenez@users.noreply.github.com>
2023-12-12 14:34:12 +0700

    Create main_popup.js

---
+++ dist/main_popup.js
@@ -0,0 +1,12 @@
+const buttons = document.querySelectorAll("button");
+buttons[0].addEventListener("Click", async() => {
+  //open AI chat box
+}
+
+buttons[1].addEventListener("Click", async() => {
+  //synchronize with Google
+}         
+                            
+buttons[2].addEventListener("Click", async() => {
+  //synchronize with Google
+}                                         
